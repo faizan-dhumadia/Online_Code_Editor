d0890f52f51ca2de3811449dc5cd39556fe84680
Faizan Dhumadia <87863621+faizan-dhumadia@users.noreply.github.com>
2022-07-12 06:43:01 +0530

    Backend Only

--- frontend/src/Service/endpoint.js
+++
@@ -1,6 +0,0 @@
-const BASE_URL = "https://judge0-ce.p.rapidapi.com";
-const getAllLanguages = "/languages";
-const getLanguageById = "/language";
-const createSubmittion = "/submissions?base64_encoded=true&fields=*";
-
-export { BASE_URL, getAllLanguages, getLanguageById, createSubmittion };
